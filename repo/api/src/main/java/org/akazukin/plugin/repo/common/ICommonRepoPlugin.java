fb67e8d3a42ed0f63d08edef9312ac08596944bf
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-03-12 11:46:20 +0900

    Restrict IRepositoryManager to IRepository types
    Updated IRepositoryManager in ICommonRepoPlugin and CommonRepoPlugin to enforce IRepository type safety.

--- repo/api/src/main/java/org/akazukin/plugin/repo/common/ICommonRepoPlugin.java
+++ repo/api/src/main/java/org/akazukin/plugin/repo/common/ICommonRepoPlugin.java
@@ -1,11 +1,12 @@
 package org.akazukin.plugin.repo.common;
 
 import org.akazukin.loader.api.context.IPlugin;
+import org.akazukin.plugin.repo.common.repo.IRepository;
 
 public interface ICommonRepoPlugin extends IPlugin {
-    IRepositoryManager<?> getRepoMgr();
+    IRepositoryManager<IRepository<?>> getRepoMgr();
 
-    void setRepoMgr(IRepositoryManager<?> repoMgr);
+    void setRepoMgr(IRepositoryManager<IRepository<?>> repoMgr);
 
     ITransactionManager getTxMgr();
 
