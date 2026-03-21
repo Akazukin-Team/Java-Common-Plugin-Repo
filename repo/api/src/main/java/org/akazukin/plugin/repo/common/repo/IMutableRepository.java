6be8c64c3573d4698c2391bdd7de6e63b464500a
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-03-22 00:01:33 +0900

    Refactor ObjectMeta with immutability split and add MutableObjectMeta
    Separated mutable functionality from ObjectMeta into a new MutableObjectMeta class for better clarity and immutability.
    Introduced IMutableObjectMeta interface.
    Removed original and isModified from ObjectMeta, moved to MutableObjectMeta.
    Updated IMutableRepository to use IMutableObjectMeta.

--- repo/api/src/main/java/org/akazukin/plugin/repo/common/repo/IMutableRepository.java
+++ repo/api/src/main/java/org/akazukin/plugin/repo/common/repo/IMutableRepository.java
@@ -1,9 +1,9 @@
 package org.akazukin.plugin.repo.common.repo;
 
-import org.akazukin.plugin.repo.common.object.IObjectMeta;
+import org.akazukin.plugin.repo.common.object.IMutableObjectMeta;
 import org.jetbrains.annotations.NotNull;
 
-public interface IMutableRepository<T extends IObjectMeta<?>> extends IRepository<T> {
+public interface IMutableRepository<T extends IMutableObjectMeta<?>> extends IRepository<T> {
     void save(@NotNull T meta);
 
     void delete(@NotNull T meta);
