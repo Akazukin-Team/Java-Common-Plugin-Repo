2f3f175713fd37e1d8455d14a73f8dd462f84837
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-03-22 00:13:03 +0900

    Replace equals with equalsData in IStoreObject
    Remove equals from IStoreObject interface.
    Introduce equalsData to explicitly handle data-based equality.

--- repo/api/src/main/java/org/akazukin/plugin/repo/common/object/IStoreObject.java
+++ repo/api/src/main/java/org/akazukin/plugin/repo/common/object/IStoreObject.java
@@ -2,8 +2,7 @@ package org.akazukin.plugin.repo.common.object;
 
 public interface IStoreObject {
     @Override
-    boolean equals(Object obj);
-
-    @Override
     String toString();
+
+    boolean equalsData(Object obj);
 }
